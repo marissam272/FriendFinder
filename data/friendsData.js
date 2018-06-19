a6bda49d0f461cfb703c453c49ff80e6c71babb0
Marissa <mharrison272@gmail.com>
2018-06-18 19:48:35 -0500

    start friendsdata

---
+++ data/friendsData.js
@@ -0,0 +1,38 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var siteFriends = [{
+    "name": "Hootie",
+    "photo": "/images/country.jpeg",
+    "scores": [
+        "1",
+        "5",
+        "5",
+        "1",
+        "4",
+        "5",
+        "3"
+    ]
+},
+
+{
+    "name": "Bro",
+    "photo": "/images/brah.jpeg",
+    "scores": [
+        "5",
+        "1",
+        "5",
+        "5",
+        "2",
+        "1",
+        "2"
+    ]
+},
+
+]
+
+module.exports = siteFriends;
